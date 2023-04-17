b20ea3ebeb8983dbd59685f6a43c09fadee0d627
bjh <qudwls5874@gmail.com>
2023-04-18 08:56:41 +0900

    comment

--- src/main/java/com/example/demo/NotepadApplication.java
+++ src/main/java/com/example/demo/NotepadApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class NotepadApplication {
 
---
+++ src/main/java/com/example/demo/config/Swagger2Config.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("triget Spring Boot REST API")
+                .version("1.0.0")
+                .description("notepad")
+                .build();
+    }
+}
